8ddea5516bbfb631664543636ab9e38ceaae45db
Severin Ulrich <sulrich@downloads.ch>
2018-10-14 21:47:53 +0200

    initial app

---
+++ app/controllers/frames_controller.rb
@@ -0,0 +1,25 @@
+class FramesController < ApplicationController
+
+  def home
+
+  end
+
+  def main_index
+    render layout: false
+  end
+
+  def sidebar
+    # links = [{url: 'http://www.google.ch', title: 'Google', group: "A"},
+    #           {url: 'http://e-request.ch', title: 'e-Request', group: "A"}]
+    links = []
+    @folders = Dir.glob("#{Rails.root}/public/docs/*/")
+    @folders.each do |folder|
+      dir = Pathname(folder).split.last.to_s
+      # Pathname.new("/my/to/somewhere/id").split.first.to_s
+      puts dir
+      links << {url: "/docs/#{dir}/", title: dir.camelcase, group: "A"}
+    end
+
+    @grouped_links = links.group_by {|i| i[:group] }
+  end
+end
---
+++ config/routes.rb
@@ -0,0 +1,10 @@
+Rails.application.routes.draw do
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  # frame sidebar
+  get 'sidebar', to: 'frames#sidebar'
+  get 'home', to: 'frames#home'
+
+  # master frame
+  root 'frames#main_index'
+end
