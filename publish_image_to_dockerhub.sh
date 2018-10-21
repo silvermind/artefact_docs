2e22c3592408e491e47911d196dbf411fa629259
Severin Ulrich <sulrich@downloads.ch>
2018-10-21 22:26:53 +0200

    fix publish link

--- publish_image_to_dockerhub.sh
+++ publish_image_to_dockerhub.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
-docker pull registry.kubernetes.ch/silvermind/artefact_docs:master-latest-docs
-docker tag registry.kubernetes.ch/silvermind/artefact_docs:master-latest-docs silvermind/artefact_docs:latest
+docker pull registry.kubernetes.ch/silvermind/artefact_docs:master-latest
+docker tag registry.kubernetes.ch/silvermind/artefact_docs:master-latest silvermind/artefact_docs:master-latest
 docker push silvermind/artefact_docs
